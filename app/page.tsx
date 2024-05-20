7bd0c3c0fc8f0df99cd9397d471cba31d3946a4e
arnnvv <arnav.900@protonmail.com>
2024-05-21 02:30:22 +0530

    reTry

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+}
