9ad7bdfefcc7cec84eb2206a080f0d171fdf71d6
arnnvv <arnav.900@protonmail.com>
2024-05-23 23:48:52 +0530

    ash

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,30 @@
+import { Icons } from "@/components/Icons";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = async ({ children }: LayoutProps) => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    notFound();
+  }
+  return (
+    <div className="w-full flex h-screen">
+      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+        <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
+          <Icons.Logo className="h-8 w-auto" />
+        </Link>
+
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
