13b4578c2fe2d39c763466420537d6ce2b944d1b
arnnvv <arnav.900@protonmail.com>
2024-05-26 19:57:11 +0530

    EmailUndefined

--- app/(dashboard)/dashboard/requests/page.tsx
+++ app/(dashboard)/dashboard/requests/page.tsx
@@ -1,4 +1,3 @@
-import FriendRequests from "@/components/FriendRequests";
 import fetchRedis from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
@@ -9,39 +8,26 @@ interface RequestsProps {}
 
 const Requests: FC<RequestsProps> = async ({}) => {
   const session = await getServerSession(authOptions);
-
   if (!session) {
     notFound();
   }
 
-  const incommingsenderIds = (await fetchRedis(
+  const incommingSenderIds = (await fetchRedis(
     `smembers`,
     `user:${session.user.id}:incoming_friend_requests`,
   )) as string[];
 
-  const incommingFriendReq = await Promise.all(
-    incommingsenderIds.map(async (senderId) => {
+  const senderEmails = await Promise.all(
+    incommingSenderIds.map(async (senderId) => {
       const sender = (await fetchRedis("get", `user:${senderId}`)) as User;
-
       return {
         senderId,
         senderEmail: sender.email,
       };
     }),
   );
-
-  return (
-    <main className="pt-8">
-      <h1 className="font-bold text-5xl mb-8">Add friend</h1>
-
-      <div className="flex flex-col gap-4">
-        <FriendRequests
-          incommingFriendReqs={incommingFriendReq}
-          sessionId={session.user.id}
-        />
-      </div>
-    </main>
-  );
+  console.log(senderEmails);
+  return <div>Requests</div>;
 };
 
 export default Requests;
